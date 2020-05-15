2409bf05169d252d70a035d15fe6fddda7adc9e3
킹지 <kokopink12@gmail.com>
2020-05-16 01:51:06 +0900

    [feat] 검색 필터 onClick 이벤트 핸들러 추가

--- src/components/Search/SearchForm.vue
+++ src/components/Search/SearchForm.vue
@@ -4,25 +4,45 @@
       <div class="row">
         <div class="left">종</div>
         <div class="right">
-          <span class="tag" v-for="(item, index) in searchFilter.species" :key="index">{{ item }}</span>
+          <span
+            class="tag"
+            v-for="(item, index) in searchFilter.species"
+            :key="index"
+            @click="onclickSpecies(item)"
+          >{{ item }}</span>
         </div>
       </div>
       <div class="row">
         <div class="left">성격</div>
         <div class="right">
-          <span class="tag" v-for="(item, index) in searchFilter.character" :key="index">{{ item }}</span>
+          <span
+            class="tag"
+            v-for="(item, index) in searchFilter.character"
+            :key="index"
+            @click="onclickCharacter(item)"
+          >{{ item }}</span>
         </div>
       </div>
       <div class="row">
         <div class="left">취미</div>
         <div class="right">
-          <span class="tag" v-for="(item, index) in searchFilter.hobby" :key="index">{{ item }}</span>
+          <span
+            class="tag"
+            v-for="(item, index) in searchFilter.hobby"
+            :key="index"
+            @click="onclickHobby(item)"
+          >{{ item }}</span>
         </div>
       </div>
       <div class="row">
         <div class="left">대화타입</div>
         <div class="right">
-          <span class="tag" v-for="(item, index) in searchFilter.talkType" :key="index">{{ item }}</span>
+          <span
+            class="tag"
+            v-for="(item, index) in searchFilter.talkType"
+            :key="index"
+            @click="onclickTalkType(item)"
+          >{{ item }}</span>
         </div>
       </div>
       <div class="row">
@@ -32,6 +52,7 @@
             class="tag"
             v-for="(item, index) in searchFilter.preferStyle1"
             :key="index"
+            @click="onclickPreferStyle(item)"
           >{{ item }}</span>
         </div>
       </div>
@@ -42,6 +63,7 @@
             class="tag"
             v-for="(item, index) in searchFilter.preferColor1"
             :key="index"
+            @click="onclickPreferColor(item)"
           >{{ item }}</span>
         </div>
       </div>
@@ -57,18 +79,80 @@ export default {
     return {
       searchFilter,
       searchForm: {
-        species: "",
-        character: "",
-        hobby: "",
-        talkType: "",
-        birth: "",
-        preferStyle1: "",
-        preferStyle2: "",
-        preferColor1: "",
-        preferColor2: "",
-        voiceType: ""
+        species: [],
+        character: [],
+        hobby: [],
+        talkType: [],
+        birth: [],
+        preferStyle1: [],
+        preferStyle2: [],
+        preferColor1: [],
+        preferColor2: [],
+        voiceType: []
       }
     };
+  },
+  methods: {
+    onclickSpecies(item) {
+      const existIndex = this.searchForm.species.findIndex(
+        element => element == item
+      );
+      if (existIndex > -1) {
+        this.searchForm.species.splice(existIndex, 1);
+      } else {
+        this.searchForm.species.push(item);
+      }
+    },
+    onclickCharacter(item) {
+      const existIndex = this.searchForm.character.findIndex(
+        element => element == item
+      );
+      if (existIndex > -1) {
+        this.searchForm.character.splice(existIndex, 1);
+      } else {
+        this.searchForm.character.push(item);
+      }
+    },
+    onclickHobby(item) {
+      const existIndex = this.searchForm.hobby.findIndex(
+        element => element == item
+      );
+      if (existIndex > -1) {
+        this.searchForm.hobby.splice(existIndex, 1);
+      } else {
+        this.searchForm.hobby.push(item);
+      }
+    },
+    onclickTalkType(item) {
+      const existIndex = this.searchForm.hobby.findIndex(
+        element => element == item
+      );
+      if (existIndex > -1) {
+        this.searchForm.talkType.splice(existIndex, 1);
+      } else {
+        this.searchForm.talkType.push(item);
+      }
+    },
+    onclickPreferStyle(item) {
+      const existIndex = this.searchForm.preferStyle1.findIndex(
+        element => element == item
+      );
+      if (existIndex > -1) {
+        this.searchForm.preferStyle1.splice(existIndex, 1);
+      } else {
+        this.searchForm.preferStyle1.push(item);
+      }
+    },
+    onclickPreferColor(item) {
+      const existIndex = this.searchForm.preferColor1.findIndex(
+        element => element == item
+      );
+      if (existIndex > -1) {
+        this.searchForm.preferColor1.splice(existIndex, 1);
+      } else {
+        this.searchForm.preferColor1.push(item);
+      }
+    }
   }
 };
 </script>
